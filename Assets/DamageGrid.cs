d626de9e9f0917353b19aed38230cf84f982897f
NilsCarron <nilscarron@yahoo.fr>
2022-11-28 16:08:11 +0100

    Adding the Grids in Json

--- Assets/DamageGrid.cs
+++ Assets/DamageGrid.cs
@@ -1,10 +1,31 @@
-﻿[System.Serializable]
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
 public class DamageGrid
 {
     public string v;
+    private string path;
+    private string jsonString;
+    public char[][] grid;
 
     public DamageGrid(string v)
     {
         this.v = v;
+        
+        // Given JSON input:
+        // {"name":"Dr Charles","lives":3,"health":0.8}
+        // this example will return a PlayerInfo object with
+        // name == "Dr Charles", lives == 3, and health == 0.8f.
     }
+
+    public char[][] CreateFromJSON(string jsonString)
+    {
+
+        return JsonUtility.FromJson<PlayerInfo>(jsonString);
+    }
+
 }
+
